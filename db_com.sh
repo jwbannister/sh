57a6fe56f62fe2a3829cfd2039a8f43f8a08e001
John Bannister <jbannister@airsci.com>
2017-08-03 15:46:27 -0600

    commit on T470 for push to pdxmod1

--- db_com.sh
+++ db_com.sh
@@ -1,16 +1,10 @@
 #!/usr/bin/env bash
-psql -h 192.168.22.42 -d owenslake -U jbannister <<EOF
+psql -h $PSQL_HOST_OWENS -d owenslake -U airsci <<EOF
 CREATE TEMP TABLE tmp AS  
-SELECT mass.coll_id, mass.site, mass.start_datetime, 
-  mass.collection_datetime, mass.sandmass, mass.catch_full, mass.catch_wet, 
-  mass.catch_spilled, idep.easting_utm AS x, idep.northing_utm AS y
-FROM sandcatches.csc_data mass
-JOIN instruments.deployments idep
-ON mass.deployment_id=idep.deployment_id
-JOIN instruments.areas ia
-ON idep.area_id=ia.area_id
-WHERE ia.description='Tillage with BACM Backup (TwB2)'
-ORDER BY mass.coll_id, mass.site;
-\copy tmp TO '/home/john/Desktop/twb2_mass.csv' (FORMAT CSV, HEADER);
+SELECT datetime, site, aspd AS ws, dir AS wd, teom AS pm10 
+FROM archive.mfile_data 
+WHERE (datetime - '1 second'::interval)::date>='2017-01-01'::date
+ORDER BY site, datetime;
+\copy tmp TO '/home/john/Desktop/mfile_data_2017' (FORMAT CSV, HEADER);
 EOF
 
---
+++ vider.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+cd $1
+tmux new -s $1 -d
+tmux split-window -h -t $1
+tmux resize-pane -t $1.0 -x 120
+tmux select-pane -t $1.0 
+tmux send-keys "vim" C-m
+tmux send-keys ":30vs" C-m
+tmux send-keys "-" C-m
+tmux attach -t $1
