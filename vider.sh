577c883c88169d0c9bf7e14e5b4388add25b37b2
John Bannister <jbannister@airsci.com>
2018-09-26 22:15:33 -0700

    modified vider.sh to work with conda environments

--- vider.sh
+++ vider.sh
@@ -1,17 +1,16 @@
-#!/bin/bash
-if [ $# -eq 1 ]; then
-    SESH=$1
-else
-    SESH=$2
-fi 
+#! /bin/zsh
 COLUMNS=$(tput cols)
-cd $1
-tmux new -s $SESH -d -x "$COLUMNS"
-tmux split-pane -h -b -l 125 -t $SESH
-tmux send-keys -t $SESH:0.0 "vim" C-m
-tmux send-keys -t $SESH:0.0 ":30vs" C-m
-tmux send-keys -t $SESH:0.0 ":Explore" C-m
+cd ~/code/$1
+. /anaconda3/etc/profile.d/conda.sh
+conda deactivate
+tmux new -s $1 -d -x "$COLUMNS"
+tmux send-keys -t $1:0.0 "conda activate $1" C-m
+tmux set-environment -t $1 TMUX_ENV $1
+tmux split-pane -h -b -l 125 -t $1
+tmux send-keys -t $1:0.0 "vim" C-m
+tmux send-keys -t $1:0.0 ":30vs" C-m
+tmux send-keys -t $1:0.0 ":Explore" C-m
 sleep 1
-tmux send-keys -t $SESH:0.0  C-l
-tmux send-keys -t $SESH:0.0  C-h
-tmux attach -t $SESH
+tmux send-keys -t $1:0.0  C-l
+tmux send-keys -t $1:0.0  C-h
+tmux attach -t $1
